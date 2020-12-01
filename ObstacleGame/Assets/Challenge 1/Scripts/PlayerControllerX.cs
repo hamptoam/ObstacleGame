a7e578985a59ea8f994216ea26569709eaabd635
Amelia Hampton <hamptoam.dev@outlook.com>
2020-11-30 22:04:51 -0600

    As per challenge, was able to make plane move forward on it's own and be able to rotate on it's axis with user input.

--- ObstacleGame/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ ObstacleGame/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,33 +4,35 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    private float speed = 20.0f;
-    private float thrust = 1.0f;
-    private float rotationSpeed = 45.0f;
-    private float verticalInput;
-    private float horizontalInput;
-    private float forwardInput;
     public Rigidbody rb;
+   
+    private float tiltSpeed = 20.0f;
+    private float thrust = 10.0f;
+    private float verticalInput;
+    public float forwardVel;
+    public float turnVel;
+
+  
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.AddForce(0, 0, thrust, ForceMode.Impulse);
+        rb.AddForce(0, 0, forwardVel, ForceMode.Impulse);
     }
+
+
+
 
     // Update is called once per frame
     void FixedUpdate()
     {
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
-
-        // move the plane forward at a constant rate
+  
+        // move the plane forward at a constant rate 
         rb.AddForce(transform.forward * thrust);
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        transform.Rotate(Vector3.up * rotationSpeed * horizontalInput * verticalInput * Time.deltaTime);
-       // transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        rb.transform.position += transform.forward * (thrust * Time.deltaTime);
+        rb.transform.Rotate(Vector3.right * tiltSpeed * verticalInput * Time.deltaTime);
     }
 }
